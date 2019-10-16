568f2f16225832f56341c9b8b3a73e729c40cf59
SergeyLebidko <it.sergeyler@mail.ru>
2019-10-16 21:43:24 +0300

    Добавлена первая форма

---
+++ main/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Topic
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['title', 'content']
+        labels = {'title': 'Тема', 'content': 'Текст'}
--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
     topics = Topic.objects.order_by('-published')
-    context = {'topics': topics}
-    return render(request, 'main/index.html', context)
+    form = TopicForm()
+    context = {'topics': topics, 'form': form}
+    if request.method == 'GET':
+        return render(request, 'main/index.html', context)
